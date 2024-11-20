d579dabd3b9dbeb59dd47c2bb077f28960cf0ab1
thessena <timhessenauer@gmail.com>
2024-11-20 09:48:49 +0100

    created headerfile

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,16 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 17:22:51 by thessena          #+#    #+#             */
-/*   Updated: 2024/11/14 17:22:52 by thessena         ###   ########.fr       */
+/*   Updated: 2024/11/20 09:47:59 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
