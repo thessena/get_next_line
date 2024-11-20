515814a0073637dac423726f5ef79aae7f4334e2
thessena <timhessenauer@gmail.com>
2024-11-20 10:09:17 +0100

    starting to implement helpfunctions

--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,21 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 17:22:51 by thessena          #+#    #+#             */
-/*   Updated: 2024/11/20 09:47:59 by thessena         ###   ########.fr       */
+/*   Updated: 2024/11/20 10:07:56 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
+#define	BUFFER_SIZE 42
 
 #include <unistd.h>
 #include <stdlib.h>
 
 char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,22 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 17:24:00 by thessena          #+#    #+#             */
-/*   Updated: 2024/11/14 17:24:01 by thessena         ###   ########.fr       */
+/*   Updated: 2024/11/20 10:08:53 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s1);
