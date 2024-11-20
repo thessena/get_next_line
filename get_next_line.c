bdb5638428a979a1d7f93911c3b7ab1955cfb57f
thessena <timhessenauer@gmail.com>
2024-11-20 13:33:48 +0100

    created extract_line function

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,33 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 17:25:59 by thessena          #+#    #+#             */
-/*   Updated: 2024/11/20 11:09:28 by thessena         ###   ########.fr       */
+/*   Updated: 2024/11/20 11:22:15 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*extract_line(char **remainder)
+{
+	char	*line;
+	char	*new_remainder;
+	size_t	line_len;
+	
+	line_len = 0;
+	while ((*remainder)[line_len] && (*remainder)[line_len] != '\n')
+		line_len++;
+	
+	line = (char *)malloc((line_len + 1 + ((*remainder)[line_len] == '\n')) * sizeof(char));
+	if (!line)
+		return (NULL);
+	ft_memcpy(line, *remainder, line_len + ((*remainder)[line_len] == '\n'));
+	line[line_len + ((*remainder)[line_len] == '\n')] = '\0';
+	new_remainder = ft_strdup(*remainder + line_len + ((*remainder)[line_len] == '\n'));
+	free(*remainder);
+	*remainder = new_remainder;
+	
+	return (line);
+}
 
 char	*get_next_line(int fd)
 {
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,15 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 17:22:51 by thessena          #+#    #+#             */
-/*   Updated: 2024/11/20 11:03:47 by thessena         ###   ########.fr       */
+/*   Updated: 2024/11/20 13:28:25 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define	BUFFER_SIZE 42
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+# endif
 
 # include <unistd.h>
 # include <stdlib.h>
@@ -22,5 +24,6 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 
 #endif
