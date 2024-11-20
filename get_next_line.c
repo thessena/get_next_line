1d204d18aa43fc223fa5ebb332a1fd86ef5b5efd
thessena <timhessenauer@gmail.com>
2024-11-20 09:57:57 +0100

    prepare memory for buffer

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 17:25:59 by thessena          #+#    #+#             */
-/*   Updated: 2024/11/20 09:52:16 by thessena         ###   ########.fr       */
+/*   Updated: 2024/11/20 09:57:09 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,18 @@
 
 char	*get_next_line(int fd)
 {
-	static char	*buffer;
+	static char	*remainder;
+	char		*buffer;
 	char		*line;
+	int			bytes_read;
 	
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	
+	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return (NULL);
+
+	free (buffer);
 	return (line);
 }
