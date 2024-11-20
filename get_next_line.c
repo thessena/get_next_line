5c29be456d84be00ebc5f31111f6a17824adff06
thessena <timhessenauer@gmail.com>
2024-11-20 15:20:09 +0100

    fixed segmentation faults

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 17:25:59 by thessena          #+#    #+#             */
-/*   Updated: 2024/11/20 14:54:03 by thessena         ###   ########.fr       */
+/*   Updated: 2024/11/20 15:18:19 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,13 @@ char	*extract_line(char **remainder)
 	size_t	line_len;
 	size_t	extra_char;
 
-	if (!remainder || !*remainder)
+	if (!remainder || !*remainder || **remainder == '\0')
+	{
+		if (remainder && *remainder)
+			free(*remainder);
+		*remainder = NULL;
 		return (NULL);
+	}
 	line_len = 0;
 	while ((*remainder)[line_len] && (*remainder)[line_len] != '\n')
 		line_len++;
@@ -44,15 +49,17 @@ char	*get_next_line(int fd)
 	int			bytes_read;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
+	{
+		if (remainder)
+		{
+			free(remainder);
+			remainder = NULL;
+		}
 		return (NULL);
-	if (!remainder)
-		remainder = ft_strdup("");
+	}
 	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buffer)
 		return (NULL);
-	if (remainder && ft_strchr(remainder, '\n'))
-		return (extract_line(&remainder));
-	line = 0;
 	bytes_read = 1;
 	while (bytes_read > 0)
 	{
@@ -60,22 +67,32 @@ char	*get_next_line(int fd)
 		if (bytes_read < 0)
 		{
 			free(buffer);
+			free(remainder);
+			remainder = NULL;
 			return (NULL);
 		}
-		buffer[bytes_read] = '\0';
+		if (bytes_read > 0)
+			buffer[bytes_read] = '\0';
+		if (!remainder)
+			remainder = ft_strdup("");
 		remainder = ft_strjoin(remainder, buffer);
-		if (ft_strchr(remainder, '\n'))
+		if (!remainder)
 		{
 			free(buffer);
-			return (extract_line(&remainder));
+			return (NULL);
+		}
+		if (ft_strchr(remainder, '\n'))
+		{
+			line = extract_line(&remainder);
+			free(buffer);
+			return (line);
 		}
 	}
 	free (buffer);
-	if (bytes_read < 0)
-		return (NULL);
-	if (remainder && *remainder != '\0')
-		return (extract_line(&remainder));
-	free(remainder);
-	remainder = NULL;
+	if (bytes_read == 0 && remainder)
+	{
+		line = extract_line(&remainder);
+		return (line);
+	}
 	return (NULL);
 }
